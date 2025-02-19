9ce79de7955514758ace9bfdaf440f8373e3eda0
kumaranal <analbiswas180@gmail.com>
2025-02-19 09:02:11 +0530

    change

--- supabase/migrations/20240115123019_unique-owner-index.sql
+++
@@ -1,2 +0,0 @@
-create unique index unique_owner on memberships(organization_id, role)
-where (role = 2);
--- supabase/migrations/20240125131904_updated.sql
+++
@@ -1,8 +0,0 @@
-create table school (
-  id bigint generated always as identity primary key,
-  name text not null,
-  address text not null,
-  contact_no bigint not null,
-  created_at timestamptz not null default now(),
-  updated_at timestamptz not null default now()
-);
